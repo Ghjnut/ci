4c0f9ea7e12c4816b36ead12f31e01cc438ea4cc
Jake Pelletier <jake.pelletier@sendgrid.com>
2016-03-23 16:34:12 -0600

    Add action to start a jenkins job

--- lib/workflow.rb
+++ lib/workflow.rb
@@ -2,11 +2,10 @@ require 'workflow/version'
 require 'workflow/jenkins'
 
 module Workflow
-  #def start_build(jenkins_job, branch=nil, commit=nil, pull_request=nil, merge_master=False)
-  def Workflow.start_job(job_name)
-		Workflow::Jenkins.start_job(job_name)
-    #build = JenkinsBuild = Jenkins.get_build(build_name)
-    #return build.start(jenkins_job, branch=nil, commit=nil, pull_request=nil, merge_master=False)
+  # def start_build(jenkins_job, branch=nil, commit=nil, pull_request=nil, merge_master=False)
+  def self.start_job(job_name)
+    Workflow::Jenkins.start_job(job_name)
+    # build = JenkinsBuild = Jenkins.get_build(build_name)
+    # return build.start(jenkins_job, branch=nil, commit=nil, pull_request=nil, merge_master=False)
   end
-
 end
