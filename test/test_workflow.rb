f91be0d18a90448f647fcc035d0037f53de2196f
Jake Pelletier <jake.pelletier@sendgrid.com>
2016-03-23 16:32:33 -0600

    :fire: Pull jenkins specific tests and add workflow tests

--- test/test_workflow.rb
+++ test/test_workflow.rb
@@ -3,48 +3,48 @@ require 'workflow'
 
 class TestWorkflow < Minitest::Test
   def test_job_start
-		# TODO: Start a jenkins job with a github PR.
+    # TODO: Start a jenkins job with a github PR.
     Workflow.start_job('test')
-    #assert_equal "OHAI!", @meme.i_can_has_cheezburger?
+    # assert_equal "OHAI!", @meme.i_can_has_cheezburger?
   end
 
   def test_job_pr_start
-		# TODO: Start a jenkins job with a github PR.
-    #Workflow.start_build('test')
-    #assert_equal "OHAI!", @meme.i_can_has_cheezburger?
+    # TODO: Start a jenkins job with a github PR.
+    # Workflow.start_build('test')
+    # assert_equal "OHAI!", @meme.i_can_has_cheezburger?
   end
 
   def test_job_pr_list
-		# TODO: Test return of list of open prs.
-    #Workflow.start_build('test')
-    #assert_equal "OHAI!", @meme.i_can_has_cheezburger?
+    # TODO: Test return of list of open prs.
+    # Workflow.start_build('test')
+    # assert_equal "OHAI!", @meme.i_can_has_cheezburger?
   end
 
   def test_build_create
-		# TODO: Create a jenkins build.
+    # TODO: Create a jenkins build.
   end
 
   def test_build_update
-		# TODO: Update a jenkins build with expected return.
+    # TODO: Update a jenkins build with expected return.
   end
 
   def test_build_update
-		# TODO: Update a jenkins build with expected return.
+    # TODO: Update a jenkins build with expected return.
   end
-  
-  #def setup
+
+  # def setup
   #  @meme = Meme.new
-  #end
+  # end
 
-  #def test_that_kitty_can_eat
+  # def test_that_kitty_can_eat
   #  assert_equal "OHAI!", @meme.i_can_has_cheezburger?
-  #end
+  # end
 
-  #def test_that_it_will_not_blend
+  # def test_that_it_will_not_blend
   #  refute_match /^no/i, @meme.will_it_blend?
-  #end
+  # end
 
-  #def test_that_will_be_skipped
+  # def test_that_will_be_skipped
   #  skip "test this later"
-  #end
+  # end
 end
