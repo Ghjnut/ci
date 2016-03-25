402e459d5c13bf35be54e745cae1ede5209b2f01
Jake Pelletier <jake.pelletier@sendgrid.com>
2016-03-25 00:40:35 -0600

    :fire: Remove unused code

--- lib/workflow/git.rb
+++ lib/workflow/git.rb
@@ -40,19 +40,4 @@ module Workflow
       @client.merge_pull_request(repo, pull_request_number)
     end
   end
-  # module Git
-  #   class Ref
-  #     branch = nil
-  #     commit = nil
-  #     pr = nil
-  #   end
-  #   class Repo
-  #     organization = ''
-  #     name = ''
-  #     url = ''
-  #     ssh_ref = ''
-  #     def merge_pr(Git::Ref)
-  #     end
-  #   end
-  # end
 end
--- test/minitest/test_workflow.rb
+++ test/minitest/test_workflow.rb
@@ -50,20 +50,4 @@ class TestWorkflow < Minitest::Test
   def test_build_update
     # TODO: Update a jenkins build with expected return.
   end
-
-  # def setup
-  #  @meme = Meme.new
-  # end
-
-  # def test_that_kitty_can_eat
-  #  assert_equal "OHAI!", @meme.i_can_has_cheezburger?
-  # end
-
-  # def test_that_it_will_not_blend
-  #  refute_match /^no/i, @meme.will_it_blend?
-  # end
-
-  # def test_that_will_be_skipped
-  #  skip "test this later"
-  # end
 end
