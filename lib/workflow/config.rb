cc563ebd80f8bc400d58dd93ee1c4843835a2c0b
Jake Pelletier <jake.pelletier@sendgrid.com>
2016-03-28 12:07:36 -0600

    Hacky config to pull out creds from tests

--- lib/workflow/config.rb
+++ lib/workflow/config.rb
@@ -1,8 +1,25 @@
+require 'yaml'
+
 module Workflow
   class Configuration
     attr_accessor :github_token
-    def initialize(env)
+    attr_accessor :workflow
+
+    def initialize(env, path)
+      load_env_config(env)
+      @workflow = load_workflow_yaml(path)
+    end
+
+    def load_env_config(env)
       @github_token = env['GITHUB_TOKEN']
+    end
+
+    def load_workflow_yaml(path)
+      YAML.load(File.read(path))
+    end
+
+    def Configuration.factory(env=ENV, workflow_path='/usr/src/app/workflow.yml')
+      new(env, workflow_path)
     end
   end
 end
