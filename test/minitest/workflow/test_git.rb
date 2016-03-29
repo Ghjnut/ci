fcae6645637afdc6fe8ea29b1eaacdcc4c0fbe09
Jake Pelletier <jake.pelletier@sendgrid.com>
2016-03-29 03:03:36 -0600

    :art: Cleanup

--- test/minitest/workflow/test_git.rb
+++ test/minitest/workflow/test_git.rb
@@ -1,35 +1,33 @@
 require 'minitest/autorun'
 require 'workflow/git'
+
+# Should only require octokit exceptions
 require 'octokit'
 
-class TestWorkflowGithub < Minitest::Test
+class TestGithub < Minitest::Test
   def setup
-    @config = Workflow::Configuration.new(ENV)
+    @config = Workflow::Configuration.factory
     @git_client = Workflow::Github.factory(@config.github_token)
   end
 
   def test_that_login_works
+    skip 'TODO'
   end
 
   def test_load_branch
+    skip 'Hits Github API'
     @git_client.load_branch('Ghjnut/ci', 'master')
   end
 
   def test_get_pull_request_success
+    skip 'Hits Github API'
     @git_client.get_pull_request('Ghjnut/ci', 1)
   end
 
   def test_merge_pull_request_failure
+    skip 'Hits Github API'
     assert_raises Octokit::NotFound do
       @git_client.merge_pull_request('Ghjnut/ci', 0)
     end
-  end
-
-  def test_that_it_will_not_blend
-    # refute_match /^no/i, @meme.will_it_blend?
-  end
-
-  def test_that_will_be_skipped
-    skip 'test this later'
   end
 end
